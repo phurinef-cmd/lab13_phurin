6bbc63c231f4e70bbbe04c7fe89915b6672ce087
Ef <phurin.ef@gmail.com>
2026-01-06 16:06:40 +0700

    lab13_1 complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,31 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    double sum = 0.0;
+    int i = 0;
+    double sum_std = 0.0;
+    double sum_geo = 1.0;
+    double sum_Har = 0.0;
+    long double max = A[0],min = A[0];
+    while(i < N){
+        sum += A[i];
+        i++;
+    }
+    for(int j = 0;j<N ;j++){
+        sum_std += pow(A[j] - sum/N,2);
+        sum_geo *= A[j];
+        sum_Har += 1/A[j];
+        if(A[j] > max) max=A[j];
+        if(A[j] < min) min=A[j]; 
+    }
+    
+    
+    B[0] = sum/N;
+    B[1] = sqrt(sum_std/N);
+    B[2] = pow(sum_geo,1.0/N);
+    B[3] = N/sum_Har;
+    B[4] = max;
+    B[5] = min;
+}
